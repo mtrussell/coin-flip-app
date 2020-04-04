df96ec094e9d88726249eff87b909239b22bca00
Matt <smtrussell@gmail.com>
2020-04-03 21:53:45 -0500

    Updates

---
+++ precache-manifest.e4f787ce77fab4afeb03a0e16c1b7496.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dbb7234b8fc38856f0b3727a2be91417",
+    "url": "/coin-flip-app/index.html"
+  },
+  {
+    "revision": "9af108628cdcdc2b2a3b",
+    "url": "/coin-flip-app/static/css/main.e5dfabcf.chunk.css"
+  },
+  {
+    "revision": "70d1409d633bab3ebd9a",
+    "url": "/coin-flip-app/static/js/2.5a484773.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/coin-flip-app/static/js/2.5a484773.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9af108628cdcdc2b2a3b",
+    "url": "/coin-flip-app/static/js/main.c1be976d.chunk.js"
+  },
+  {
+    "revision": "0b7f27ac1e0a5c129084",
+    "url": "/coin-flip-app/static/js/runtime-main.4553ad98.js"
+  },
+  {
+    "revision": "74d5c9f8b004fe632e4e60d5521b493c",
+    "url": "/coin-flip-app/static/media/bitcoin.74d5c9f8.jpg"
+  },
+  {
+    "revision": "520a65efd2bd98ac9eaa3c9b1d6c47b2",
+    "url": "/coin-flip-app/static/media/ethereum.520a65ef.jpg"
+  }
+]);
